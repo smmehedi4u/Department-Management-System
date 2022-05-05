598c71b5191187fe2cba15939a167013e28a7673
Amsiam <amsiam990@gmail.com>
2022-05-06 01:12:24 +0600

    notice add page

--- resources/views/notice/create.blade.php
+++ resources/views/notice/create.blade.php
@@ -33,8 +33,10 @@
                                 class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none "
                                 aria-describedby="user_avatar_help" id="file" type="file">
                         </div>
+
                         <button type="submit"
-                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center ">Submit</button>
+                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center ">
+                            Add </button>
                     </form>
 
 
