cd2ab690d6cc3b018c4953771468dde691eb5809
Amsiam <amsiam990@gmail.com>
2022-05-06 01:09:13 +0600

    added flowbite and notice page

--- resources/views/notice/create.blade.php
+++ resources/views/notice/create.blade.php
@@ -12,26 +12,26 @@
 
                     <form>
                         <div class="mb-6">
-                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Your
-                                email</label>
-                            <input type="email" id="email"
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">
+                                Notice Title
+                            </label>
+                            <input autocomplete="off" autofocus="true" type="text" id="title"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5   "
-                                placeholder="name@flowbite.com" required="">
+                                placeholder="Enter Title Here" required="">
                         </div>
                         <div class="mb-6">
-                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 ">Your
-                                password</label>
-                            <input type="password" id="password"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5   "
-                                required="">
+                            <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">
+                                Notice Details
+                            </label>
+                            <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
+                                placeholder="Details"></textarea>
                         </div>
-                        <div class="flex items-start mb-6">
-                            <div class="flex items-center h-5">
-                                <input id="remember" type="checkbox" value=""
-                                    class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300  dark:focus:ring-blue-600 "
-                                    required="">
-                            </div>
-                            <label for="remember" class="ml-2 text-sm font-medium text-gray-900 ">Remember me</label>
+                        <div class="mb-6">
+                            <label class="block mb-2 text-sm font-medium text-gray-900" for="user_avatar">Upload
+                                file</label>
+                            <input
+                                class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none "
+                                aria-describedby="user_avatar_help" id="file" type="file">
                         </div>
                         <button type="submit"
                             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center ">Submit</button>
